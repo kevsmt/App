402824cd311f3385a17e77fb2419e7d067fe0208
Kevinralph Tenorio <khebs@live.com>
2011-08-31 01:56:06 +0800

    Firefox Hot Fix

--- app/views/Viewport.js
+++ app/views/Viewport.js
@@ -3,40 +3,7 @@
 	var template = App.Template.load;
 
 	var view = Backbone.View.extend({
-		el: '#app-root-view',
-
-		initialize: function() {
-			// Check browser compatibility
-			if($.browser.webkit || $.browser.mozilla) {
-				this._showViewport();
-			} else {
-				this._notCompatible();
-			}
-		},
-
-		_showViewport: function() {
-			template(App.configs.urls.templates + '/viewport', null, this.el);
-		},
-
-		_notCompatible: function() {
-			template(App.configs.urls.templates + '/common', {
-				title: 'BROWSER NOT COMPATIBLE',
-				image: App.assetUrl('img/icons/x.png'),
-				message: 'Sorry your browser is not compatible.%n%n~ Dev'
-			}, this.el, function() {
-				App.Viewport.show();
-			});
-
-		},
-
-		show: function() {
-			return $(this.el).css('visibility', 'visible');
-		},
-
-		hide: function() {
-			return $(this.el).css('visibility', 'hidden');
-		}
-
+		el: '#app-root-view'
 	});
 
 	App.Viewport = new view();
