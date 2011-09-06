75801c1ef7919e5de84b1473d820e1ecd9a0da5b
Kevinralph Tenorio <khebs@live.com>
2011-09-07 03:29:58 +0800

    Refactored All

--- app/lib/templates.js
+++
@@ -1,9 +0,0 @@
-// Message Template
-App.addTpl('message', './app/templates/common');
-
-// Page 404 Template
-App.addTpl('page404', './app/templates/common', {
-	title: 'Page Not Found',
-	image: App.assetUrl('img/icons/warning.png'),
-	message: 'Page not found'
-});
--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 <html>
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
-		<link rel="stylesheet" type="text/css" href="app/assets/css/reset.css">
-		<link rel="stylesheet/less" type="text/css" href="app/assets/less/theme-blue.less">
+		<link rel="stylesheet" type="text/css" href="modules/app/assets/css/reset.css">
+		<link rel="stylesheet/less" type="text/css" href="modules/app/assets/less/theme-blue.less">
 		<!-- Required -->
 		<script src="lib/deps/vendor/less.js" type="text/javascript"></script>
 		<script src="lib/deps/vendor/head.js" type="text/javascript"></script>
 		<script src="lib/deps/vendor/jquery/jquery.js" type="text/javascript"></script>
+		<script src="lib/deps/vendor/jquery/plugins/tmpl.js" type="text/javascript"></script>
+		<script src="lib/deps/vendor/underscore/underscore.js" type="text/javascript"></script>
+		<script src="lib/deps/vendor/underscore/plugins/string.js" type="text/javascript"></script>
+		<script src="lib/deps/vendor/backbone/backbone.js" type="text/javascript"></script>
 		<!-- Main App Loader -->
-		<script src="app/app.js?v1" type="text/javascript"></script>
+		<script src="lib/app.js" type="text/javascript"></script>
 		<title></title>
 	</head>
 	<body>
